336b07ef87a71b7cc3ac58f038c5e4667158d8f3
Paolo Carrasco <paolo.carrasco@gmail.com>
2017-07-14 03:28:21 -0500

    search shows loading bar while fetching

--- src/home/index.js
+++ src/home/index.js
@@ -1,21 +1,24 @@
 import { inject, bindable } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
 import { ItunesService } from '../services/itunes-service';
 
-@inject(ItunesService)
+@inject(ItunesService, EventAggregator)
 export class HomeIndex {
 
   @bindable() filterText = '';
 
-  constructor(service) {
+  constructor(service, eventAggregator) {
     this.service = service;
+    this.eventAggregator = eventAggregator;
   }
 
   filterTextChanged(oldValue, newValue) {
+    this.eventAggregator.publish('search.started');
     this.service
       .getSongsByText(this.filterText)
-      .then((result) => {
-        this.result = result;
-      })
+        .then(result => this.result = result)
+        .catch(error => console.log(error))
+        .then(() => this.eventAggregator.publish('search.finished'));
   }
 
 }
--- src/search-bar/search-bar.js
+++ src/search-bar/search-bar.js
@@ -1,10 +1,29 @@
-import { bindable } from 'aurelia-framework';
+import { inject, bindable } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
+@inject(EventAggregator)
 export class SearchBar {
 
   @bindable() filterText = '';
   @bindable query = '';
+
   timeoutHandle;
+
+  constructor(eventAggregator) {
+    this.eventAggregator = eventAggregator;
+  }
+
+  attached() {
+    this.searchStartedSubscription = this.eventAggregator
+      .subscribe('search.started', this.onSearchStart.bind(this));
+    this.searchFinishedSubscription = this.eventAggregator
+      .subscribe('search.finished', this.onSearchFinish.bind(this));
+  }
+
+  detached() {
+      this.searchStartedSubscription.dispose();
+      this.searchFinishedSubscription.dispose();
+  }
 
   executeSearch() {
     clearTimeout(this.timeoutHandle);
@@ -14,5 +33,13 @@ export class SearchBar {
   queryChanged(oldValue, newValue) {
     clearTimeout(this.timeoutHandle);
     this.timeoutHandle = setTimeout(() => this.executeSearch(), 400);
+  }
+
+  onSearchStart() {
+    this.loading = true;
+  }
+
+  onSearchFinish() {
+    this.loading = false;
   }
 }
